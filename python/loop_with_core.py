b74e96b9664588691648867440ec2dd99e94d5c4
Nick Amin <amin.nj@gmail.com>
2018-07-05 23:29:57 -0700

    actually use core to show it works

--- python/loop_with_core.py
+++ python/loop_with_core.py
@@ -1,4 +1,3 @@
-import numpy as np
 from tqdm import tqdm
 import ROOT as r
 import os
@@ -7,61 +6,33 @@ ch = r.TChain("Events")
 ch.Add("../508B6DBB-9742-E811-BA65-A4BF0112BCB0.root")
 # ch.Add("root://cmsxrootd.fnal.gov///store/data/Run2017F/DoubleMuon/NANOAOD/31Mar2018-v1/20000/B8AAFAB7-B248-E811-BD17-001E67248138.root")
 
-r.gROOT.ProcessLine(".L ../NanoCORE/Nano.cc+")
-tas = r.tas
-nt = r.nt
+r.gSystem.Load('../NanoCORE/NANO_CORE.so')
+for include in ["Nano.cc", "SSSelections.cc", "MetSelections.cc"]:
+    r.gInterpreter.ProcessLine('#include "../NanoCORE/%s"' % include)
 
-# FIXME
+# CORE functions will be ROOT object members after we gSystem.Load CORE
+from ROOT import nt, passesMETfiltersMoriond17, isGoodMuon
+
 nt.Init(ch)
+N = ch.GetEntries()
 
-# for evt in ch: 
-#     print evt  
-#     print 
-#     break
-
-h1 = r.TH1F("ht","",100,0,800)
-
-blah
-
-xxx term=reverse cterm=underline ctermfg=204 gui=underline guifg=#E06C75
-
-# import numba
-# @numba.njit
-# def get_ht(blah):
-#     return sum(blah)
+h1 = r.TH1F("h1","",100,0,200)
 
 def process_event(ievent):
     nt.GetEntry(ievent)
 
-    ht = 0.
-
-    # for p4 in nt.Jet_p4():
-    #     ht += p4.pt()
-
-    for x in tas.Jet_pt():
-        ht += x
-    # print get_ht(tas.Jet_pt())
-
-    h1.Fill(ht)
+    for imu,pt in enumerate(nt.Muon_pt()):
+        if pt < 25: continue
+        if not isGoodMuon(imu): continue
+        h1.Fill(pt)
 
 
-    # ht = sum(nt.Jet_pt())
-    # h1.Fill(ht)
-
-    # ht = sum(nt.Jet_pt())
-
-
-import random
-# for ievent in tqdm(range(ch.GetEntries())[::-2]):
-# blah = range(ch.GetEntries())
-# random.shuffle(blah)
-# for ievent in tqdm(blah):
 for ievent in tqdm(range(ch.GetEntries())):
-# for ievent in tqdm(range(10)):
     process_event(ievent)
-    # break
+
+print(h1.GetMean())
 
 c1 = r.TCanvas()
 h1.Draw()
 c1.SaveAs("plot.pdf")
-os.system("ic plot.pdf")
+os.system("which ic && ic plot.pdf")

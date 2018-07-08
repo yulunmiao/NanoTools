b4fad42289fe47126e8af004995bb345e4e3f3ee
Nick Amin <amin.nj@gmail.com>
2018-07-07 23:16:50 -0700

    cleanup

--- python/utils/test_jagged_operations.py
+++ python/utils/test_jagged_operations.py
@@ -8,6 +8,7 @@ import uproot
 
 
 f = uproot.open("../../508B6DBB-9742-E811-BA65-A4BF0112BCB0.root")
+# f = uproot.open("../../6204E92D-C712-E811-AF42-E0071B740D80.root")
 t = f["Events"]
 
 t0 = time.time()
@@ -17,7 +18,11 @@ print (time.time()-t0)
 pts = info["Electron_pt"]
 offsets = pts.offsets
 
-from jagged_operations import *
+from jagged_operations import jagged_foreach_function, \
+        jagged_foreach_count, jagged_foreach_count_if, \
+        jagged_foreach_sum, jagged_foreach_sum_if, \
+        jagged_foreach_min, jagged_foreach_min_if, \
+        jagged_foreach_max, jagged_foreach_max_if
 
 def test_operation(name,func_fast,func_slow, jagged,masks=None):
     offsets = jagged.offsets
@@ -50,46 +55,18 @@ mymin = lambda x: 0. if not len(x) else np.min(x)
 mymax = lambda x: 0. if not len(x) else np.max(x)
 
 
-epts = info["Electron_pt"]
-jpts = info["Jet_pt"]
-jidx = info["Electron_jetIdx"]
 
-matched_jpts = jagged_foreach_index(jpts.content,jpts.offsets,jidx.content,jidx.offsets,jidx.content>=0)
-ptratios = epts.content/matched_jpts
-print ptratios
-print (ptratios).min()
-print (ptratios).max()
-print (ptratios).mean()
-print (ptratios < -99).sum()
-print jidx.content.min()
-
-print ptratios[jidx.content < 0]
-print ptratios[jidx.content >= 0]
-
-# print epts
-# print jpts
-# print jidx
-
-sys.blah
 
 # print jagged_foreach_function(lambda x: np.sum(x))(pts.content, pts.offsets)
 # print jagged_operation(pts, "func", func=lambda x: np.sum(x))
-# test_operation("func", jagged_foreach_function(mymin), mymin, pts)
-# test_operation("count", jagged_foreach_count, len, pts)
-# test_operation("sum", jagged_foreach_sum, sum, pts)
-# test_operation("min", jagged_foreach_min, mymin, pts)
-# test_operation("max", jagged_foreach_max, mymax, pts)
-# test_operation("countif", jagged_foreach_count_if, len, pts, masks=np.ones(len(pts.content))>0)
-# test_operation("sumif", jagged_foreach_sum_if, sum, pts, masks=np.ones(len(pts.content))>0)
-# test_operation("minif", jagged_foreach_min_if, mymin, pts, masks=np.ones(len(pts.content))>0)
-# test_operation("maxif", jagged_foreach_max_if, mymax, pts, masks=np.ones(len(pts.content))>0)
 
-
-# accums_fast = jagged_foreach_count(content,offsets)
-# t0 = time.time()
-# accums_fast = jagged_foreach_count(content,offsets)
-# t1 = time.time()
-# accums_slow = np.array(map(len,pts))
-# t2 = time.time()
-# print t1-t0,t2-t1,(accums_slow-accums_fast).sum()
+test_operation("func", jagged_foreach_function(mymin), mymin, pts)
+test_operation("count", jagged_foreach_count, len, pts)
+test_operation("sum", jagged_foreach_sum, sum, pts)
+test_operation("min", jagged_foreach_min, mymin, pts)
+test_operation("max", jagged_foreach_max, mymax, pts)
+test_operation("countif", jagged_foreach_count_if, len, pts, masks=np.ones(len(pts.content))>0)
+test_operation("sumif", jagged_foreach_sum_if, sum, pts, masks=np.ones(len(pts.content))>0)
+test_operation("minif", jagged_foreach_min_if, mymin, pts, masks=np.ones(len(pts.content))>0)
+test_operation("maxif", jagged_foreach_max_if, mymax, pts, masks=np.ones(len(pts.content))>0)
 

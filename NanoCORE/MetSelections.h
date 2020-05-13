7a9a9e2a4d942cfb9c858443a66619424ae42cab
Nick Amin <amin.nj@gmail.com>
2020-05-13 01:48:32 -0700

    sorry cleanup

--- NanoCORE/MetSelections.h
+++ NanoCORE/MetSelections.h
@@ -7,12 +7,12 @@
 #include "Tools/jetcorr/FactorizedJetCorrector.h"
 #include "Tools/jetcorr/JetCorrectionUncertainty.h"
 
-#include "Tools/jetcorr/Utilities.icc"
 #include "Tools/jetcorr/JetCorrectionUncertainty.icc"
 #include "Tools/jetcorr/SimpleJetCorrectionUncertainty.icc"
+#include "Tools/jetcorr/Utilities.icc"
 
-
-pair <float, float> getT1CHSMET(FactorizedJetCorrector * jet_corrector, JetCorrectionUncertainty* jecUnc = NULL, bool uncUp = 0);
+pair<float, float> getT1CHSMET(FactorizedJetCorrector *jet_corrector, JetCorrectionUncertainty *jecUnc = NULL,
+                               bool uncUp = 0);
 bool passesMETfilters(bool isData);
 
 #endif

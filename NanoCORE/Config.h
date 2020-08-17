de2ab1e77dda29836ace1be938a7c7d4f8894e4d
Jonathan Guiang <jkguiang@gmail.com>
2020-08-17 11:36:34 -0700

    changed style

--- NanoCORE/Config.h
+++ NanoCORE/Config.h
@@ -12,53 +12,53 @@
 */
 
 class GlobalConfig {
- public:
-  int year = 0;
-  int nanoAOD_ver = 0; // 74, 80, 94, 101, ...
-  bool is_data = false;
-  bool is_signal = false;
-  float lumi = 0;
-  std::string samptype = "";  // to be moved to enum
-  std::string analysis = ""; 
+  public:
+    int year = 0;
+    int nanoAOD_ver = 0; // 74, 80, 94, 101, ...
+    bool is_data = false;
+    bool is_signal = false;
+    float lumi = 0;
+    std::string samptype = ""; // to be moved to enum
+    std::string analysis = "";
 
-  // Control for this class
-  bool verbose = false;
+    // Control for this class
+    bool verbose = false;
 
-  // JECs
-  std::string jecEraA;
-  std::string jecEraB;
-  std::string jecEraC;
-  std::string jecEraD;
-  std::string jecEraE;
-  std::string jecEraF;
-  std::string jecEraG;
-  std::string jecEraH;
-  std::string jecEraMC;
+    // JECs
+    std::string jecEraA;
+    std::string jecEraB;
+    std::string jecEraC;
+    std::string jecEraD;
+    std::string jecEraE;
+    std::string jecEraF;
+    std::string jecEraG;
+    std::string jecEraH;
+    std::string jecEraMC;
 
-  //_________________________________
-  // B-tag working points
-  float WP_DeepFlav_tight  = -1;
-  float WP_DeepFlav_medium = -1;
-  float WP_DeepFlav_loose  = -1;
+    //_________________________________
+    // B-tag working points
+    float WP_DeepFlav_tight = -1;
+    float WP_DeepFlav_medium = -1;
+    float WP_DeepFlav_loose = -1;
 
-  float WP_DeepCSV_tight  = -1;
-  float WP_DeepCSV_medium = -1;
-  float WP_DeepCSV_loose  = -1;
+    float WP_DeepCSV_tight = -1;
+    float WP_DeepCSV_medium = -1;
+    float WP_DeepCSV_loose = -1;
 
-  float WP_CSVv2_tight  = -1;
-  float WP_CSVv2_medium = -1;
-  float WP_CSVv2_loose  = -1;
+    float WP_CSVv2_tight = -1;
+    float WP_CSVv2_medium = -1;
+    float WP_CSVv2_loose = -1;
 
-  // btagSF version (filename)
-  std::string fn_btagSF_DeepFlav;
-  std::string fn_btagSF_DeepCSV;
-  std::string fn_btagSF_CSVv2;
+    // btagSF version (filename)
+    std::string fn_btagSF_DeepFlav;
+    std::string fn_btagSF_DeepCSV;
+    std::string fn_btagSF_CSVv2;
 
-  // Centrally maintained function
-  void GetConfigs(int year = 0);  // pass in year or nothing
-  void GetSampleType(std::string dsname); // reduce the sample name to shorter sample types
-  void GetConfigsFromDatasetName(std::string dsname);  // pass in the datasetname or filename (that contains the dataset info)
-
+    // Centrally maintained function
+    void GetConfigs(int year = 0);          // pass in year or nothing
+    void GetSampleType(std::string dsname); // reduce the sample name to shorter sample types
+    void GetConfigsFromDatasetName(
+        std::string dsname); // pass in the datasetname or filename (that contains the dataset info)
 };
 
 #ifndef __CINT__

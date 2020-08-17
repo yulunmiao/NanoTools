b6ba74f209590528e5fbfd137ef5cbee2c133472
Jonathan Guiang <jkguiang@gmail.com>
2020-08-17 11:58:28 -0700

    updated to use SS namespace

--- NanoCORE/SSSelections.h
+++ NanoCORE/SSSelections.h
@@ -1,28 +1,16 @@
 #ifndef SSSELECTIONS_H
 #define SSSELECTIONS_H
-#include "IsolationTools.h"
 #include "Nano.h"
+#include "Base.h"
 
-enum IDLevel
-{
-    IDdefault = -1,
-    IDveto = 0, // for Z-veto
-    IDfakablenoiso = 1,
-    IDfakable = 2, // for fake background + jet cleaning
-    IDtightnoiso = 3,
-    IDtight = 4 // for analysis
-};
+SS::IDLevel whichLeptonLevel(int id, int idx);
 
-IDLevel whichLeptonLevel(int id, int idx);
-
-struct Lepton
-{
-    Lepton(int id = 0, unsigned int idx = 0) : id_(id), idx_(idx)
-    {
-        if (id != 0)
-        {
+struct Lepton {
+    Lepton(int id = 0, unsigned int idx = 0) : id_(id), idx_(idx) {
+        if (id != 0) {
             pt_ = (abs(id_) == 11 ? nt.Electron_pt()[idx_] : nt.Muon_pt()[idx_]);
             eta_ = (abs(id_) == 11 ? nt.Electron_eta()[idx_] : nt.Muon_eta()[idx_]);
+            phi_ = (abs(id_) == 11 ? nt.Electron_phi()[idx_] : nt.Muon_phi()[idx_]);
             p4_ = (abs(id_) == 11 ? nt.Electron_p4()[idx_] : nt.Muon_p4()[idx_]);
             idlevel_ = whichLeptonLevel(id_, idx_);
         }
@@ -38,11 +26,13 @@ struct Lepton
     LorentzVector p4() { return p4_; }
     float pt() { return pt_; }
     float eta() { return eta_; }
+    float phi() { return phi_; }
 
   private:
     int id_;
     float pt_ = 0.;
     float eta_ = 0.;
+    float phi_ = 0.;
     LorentzVector p4_;
     unsigned int idx_;
     int idlevel_ = IDdefault;
@@ -50,31 +40,21 @@ struct Lepton
 typedef std::pair<Lepton, Lepton> Hyp;
 typedef std::vector<Lepton> Leptons;
 
-std::ostream &operator<<(std::ostream &os, Lepton &lep)
-{
+std::ostream &operator<<(std::ostream &os, Lepton &lep) {
     std::string lepstr = (abs(lep.id()) == 11) ? "Electron" : "Muon";
-    return os << "<" << lepstr << " id=" << std::showpos << setw(3) << lep.id() << std::noshowpos
-              << ", idx=" << setw(2) << lep.idx() << ", level=" << lep.idlevel() << ", (pT,eta)="
+    return os << "<" << lepstr << " id=" << std::showpos << setw(3) << lep.id() << std::noshowpos << ", idx=" << setw(2)
+              << lep.idx() << ", level=" << lep.idlevel() << ", (pT,eta)="
               << "(" << lep.pt() << "," << lep.eta() << ")>";
 }
-template <typename T1, typename T2> std::ostream &operator<<(std::ostream &os, std::pair<T1, T2> &p)
-{
+template <typename T1, typename T2> std::ostream &operator<<(std::ostream &os, std::pair<T1, T2> &p) {
     return os << "(" << p.first << ", " << p.second << ")";
 }
 
 vector<Lepton> getLeptons();
 std::tuple<int, int, float> getJetInfo(vector<Lepton> &leps, int variation = 0);
-std::pair<int, Hyp> getBestHyp(vector<Lepton> &leptons);
-bool isTriggerSafenoIso_v1(int iel);
-bool isTriggerSafeIso_v1(int iel);
-bool passesElectronMVA(int idlevel, int iel);
-bool isGoodMuon(int imu);
-bool isGoodElectron(int iel);
-bool isVetoMuon(int imu);
-bool isVetoElectron(int iel);
-bool isFakableMuon(int imu);
-bool isFakableElectron(int iel);
-bool isLeptonLevel(IDLevel idlevel, int id, int idx);
+std::pair<int, int> makesResonance(Leptons &leps, Lepton lep1, Lepton lep2, float mass, float window);
+std::pair<int, Hyp> getBestHyp(vector<Lepton> &leptons, bool verbose);
+bool isLeptonLevel(SS::IDLevel idlevel, int id, int idx);
 void dumpLeptonProperties(Lepton lep);
 
 #endif

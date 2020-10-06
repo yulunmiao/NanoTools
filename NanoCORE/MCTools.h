46332e789bf50661c484ca797284c4a53217b2f1
Philip Chang <sgnoohc@gmail.com>
2020-10-06 16:12:36 -0700

    Re organize

--- NanoCORE/MCTools.h
+++ NanoCORE/MCTools.h
@@ -13,25 +13,8 @@
 using namespace std;
 using namespace tas;
 
-vector<int> GenPart_leptDaughters(int i){ 
-    vector<int> lept_daughters;
-    for (unsigned int j = 0; j < nGenPart(); j++) {
-        if (GenPart_genPartIdxMother().at(j) == i && (abs(GenPart_pdgId().at(j)) >= 11 && abs(GenPart_pdgId().at(j)) <= 18) ){
-            lept_daughters.push_back(j);
-        }
-    }
-    return lept_daughters;
-}
-
-vector<int> GenPart_daughters(int i){ 
-    vector<int> daughters;
-    for (unsigned int j = 0; j < nGenPart(); j++) {
-        if (GenPart_genPartIdxMother().at(j) == i){
-            daughters.push_back(j);
-        }
-    }
-    return daughters;
-}
+vector<int> GenPart_leptDaughters(int i);
+vector<int> GenPart_daughters(int i);
 
 class GenPart {
     public:
@@ -49,45 +32,9 @@ class GenPart {
       GenPart(int idx);
 };
 
-GenPart::GenPart(int idx) {
-    idx = idx;
-    pdg_id = GenPart_pdgId().at(idx);
-    is_last = (GenPart_statusFlags().at(idx) & (1<<13)) == (1<<13);
-    is_fromHard = (GenPart_statusFlags().at(idx) & (1<<8)) == (1<<8);
-    daughters = GenPart_daughters(idx);
-    mother_idx = GenPart_genPartIdxMother().at(idx);
-    status = GenPart_status().at(idx);
-    pt = GenPart_pt().at(idx);
-    eta = GenPart_eta().at(idx);
-    phi = GenPart_phi().at(idx);
-    mass = GenPart_mass().at(idx);
-}
+bool is_bad_copy(GenPart part);
 
-bool is_bad_copy(GenPart part) {
-    return (part.pdg_id == GenPart_pdgId().at(part.daughters[0]));
-}
-
-vector<GenPart> get_GenParticles() {
-    vector<GenPart> particles;
-    for (unsigned int i = 0; i < nGenPart(); i++) {
-        GenPart part(i);
-        particles.push_back(part);
-    }
-        
-    // Make Corrections for Bad Copies
-    for (size_t i = 0; i < particles.size(); i++) {
-        int m_id = particles[i].mother_idx;
-        if (m_id >= 0){
-            if (is_bad_copy(particles[m_id])){
-                particles[i].mother_idx = particles[m_id].mother_idx;                    
-                for (size_t j = 0; j < particles[m_id].daughters.size(); j++){
-                    particles[particles[m_id].mother_idx].daughters.push_back(particles[m_id].daughters[j]);
-                }
-            }            
-        }
-    }
-    return particles;
-}
+vector<GenPart> get_GenParticles();
 
 //Parentage functions
 int dumpDocLines();

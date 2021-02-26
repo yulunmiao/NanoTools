6fee2dbcd68903634e532d75c55cabf25dd5b3df
Jonathan Guiang <jkguiang@gmail.com>
2021-02-26 12:02:47 -0800

    added simple tau selections

---
+++ NanoCORE/TauSelections.h
@@ -0,0 +1,36 @@
+#ifndef TAUSELECTIONS_H
+#define TAUSELECTIONS_H
+#include "Base.h"
+#include "Nano.h"
+#include <math.h>
+
+namespace SS {
+    bool tauID(int idx, SS::IDLevel id_level, int year);
+    bool tau2016ID(int idx, SS::IDLevel id_level);
+    bool tau2017ID(int idx, SS::IDLevel id_level);
+    bool tau2018ID(int idx, SS::IDLevel id_level);
+}
+
+namespace DeepTau {
+    enum IDLevel {
+        vvvLoose,
+        vvLoose,
+        vLoose,
+        loose,
+        medium,
+        tight,
+        vTight,
+        vvTight,
+        vvvTight
+    };
+
+    enum Variant {
+        vsElectrons,
+        vsMuons,
+        vsJets
+    };
+}
+
+bool passesDeepTau(int idx, DeepTau::IDLevel id_level, DeepTau::Variant variant);
+
+#endif
